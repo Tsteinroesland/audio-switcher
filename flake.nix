65c0c788e90f07be55fd6a8cdd9732f033bef5c8
Torstein Røsland <tsteinroesland@gmail.com>
2025-07-02 11:25:59 +0200

    ADd flake

---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  description = "Audio device switcher using Babashka and wpctl";
+  
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        
+        audio-switcher = pkgs.writeShellScriptBin "audio-switcher" ''
+          #!/usr/bin/env bb
+          
+          ${builtins.readFile ./audio-switcher.clj}
+        '';
+        
+      in {
+        # Make the script available as a package
+        packages.default = audio-switcher;
+        packages.audio-switcher = audio-switcher;
+        
+        # Development shell (minimal since deps are system-wide)
+        devShells.default = pkgs.mkShell {
+          buildInputs = [ ];
+        };
+        
+        # NixOS module for easy integration
+        nixosModules.default = { config, lib, pkgs, ... }: {
+          options.programs.audio-switcher = {
+            enable = lib.mkEnableOption "audio switcher script";
+          };
+          
+          config = lib.mkIf config.programs.audio-switcher.enable {
+            environment.systemPackages = [ 
+              self.packages.${system}.audio-switcher 
+            ];
+          };
+        };
+        
+        # App entry for desktop integration
+        apps.default = {
+          type = "app";
+          program = "${audio-switcher}/bin/audio-switcher";
+        };
+      });
+}
