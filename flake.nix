0f1d7291978240db6e5a596d6bd6cb07d65af8a7
Torstein Røsland <tsteinroesland@gmail.com>
2025-07-02 11:56:21 +0200

    Update stuff

--- flake.nix
+++ flake.nix
@@ -3,52 +3,53 @@
   
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs }:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-      
-      audio-switcher = pkgs.writeShellScriptBin "audio-switcher" ''
-        #!/usr/bin/env bb
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
         
-        ${builtins.readFile ./audio-switcher.clj}
-      '';
-      
-    in {
-      # Make the script available as a package
-      packages.${system} = {
-        default = audio-switcher;
-        audio-switcher = audio-switcher;
-      };
-              
-      # Development shell (minimal since deps are system-wide)
-      devShells.${system}.default = pkgs.mkShell {
-        buildInputs = [ ];
-      };
-      
-      # NixOS module for easy integration
-      nixosModules.default = { config, lib, pkgs, ... }: {
-        options.programs.audio-switcher = {
-          enable = lib.mkEnableOption "audio switcher script";
-        };
+        audio-switcher = pkgs.writeShellScriptBin "audio-switcher" ''
+          export PATH=${pkgs.lib.makeBinPath [ 
+            pkgs.babashka 
+            pkgs.wireplumber  # provides wpctl
+          ]}:$PATH
+          
+          exec ${pkgs.babashka}/bin/bb ${./audio-switcher.clj} "$@"
+        '';
         
-        config = lib.mkIf config.programs.audio-switcher.enable {
-          environment.systemPackages = [ 
-            self.packages.${system}.audio-switcher 
+      in {
+        # Make the script available as a package
+        packages.default = audio-switcher;
+        packages.audio-switcher = audio-switcher;
+        
+        # Development shell
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            babashka
+            wireplumber
           ];
         };
-      };
-      
-      # App entry for desktop integration
-      apps.${system}.default = {
-        type = "app";
-        program = "${audio-switcher}/bin/audio-switcher";
-        meta = {
-          description = "Toggle between built-in audio and SteelSeries headset";
-          platforms = [ "x86_64-linux" ];
+        
+        # NixOS module for easy integration
+        nixosModules.default = { config, lib, pkgs, ... }: {
+          options.programs.audio-switcher = {
+            enable = lib.mkEnableOption "audio switcher script";
+          };
+          
+          config = lib.mkIf config.programs.audio-switcher.enable {
+            environment.systemPackages = [ 
+              self.packages.${system}.audio-switcher 
+            ];
+          };
         };
-      };
-    };
+        
+        # App entry for desktop integration
+        apps.default = {
+          type = "app";
+          program = "${audio-switcher}/bin/audio-switcher";
+        };
+      });
 }
