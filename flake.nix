26182c61ed0cdcabf156a7d7baf1c41e70118136
Torstein Røsland <tsteinroesland@gmail.com>
2025-07-02 11:30:15 +0200

    Update flake

--- flake.nix
+++ flake.nix
@@ -3,47 +3,52 @@
   
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      
+      audio-switcher = pkgs.writeShellScriptBin "audio-switcher" ''
+        #!/usr/bin/env bb
         
-        audio-switcher = pkgs.writeShellScriptBin "audio-switcher" ''
-          #!/usr/bin/env bb
-          
-          ${builtins.readFile ./audio-switcher.clj}
-        '';
-        
-      in {
-        # Make the script available as a package
-        packages.default = audio-switcher;
-        packages.audio-switcher = audio-switcher;
-        
-        # Development shell (minimal since deps are system-wide)
-        devShells.default = pkgs.mkShell {
-          buildInputs = [ ];
+        ${builtins.readFile ./audio-switcher.clj}
+      '';
+      
+    in {
+      # Make the script available as a package
+      packages.${system} = {
+        default = audio-switcher;
+        audio-switcher = audio-switcher;
+      };
+              
+      # Development shell (minimal since deps are system-wide)
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = [ ];
+      };
+      
+      # NixOS module for easy integration
+      nixosModules.default = { config, lib, pkgs, ... }: {
+        options.programs.audio-switcher = {
+          enable = lib.mkEnableOption "audio switcher script";
         };
         
-        # NixOS module for easy integration
-        nixosModules.default = { config, lib, pkgs, ... }: {
-          options.programs.audio-switcher = {
-            enable = lib.mkEnableOption "audio switcher script";
-          };
-          
-          config = lib.mkIf config.programs.audio-switcher.enable {
-            environment.systemPackages = [ 
-              self.packages.${system}.audio-switcher 
-            ];
-          };
+        config = lib.mkIf config.programs.audio-switcher.enable {
+          environment.systemPackages = [ 
+            self.packages.${system}.audio-switcher 
+          ];
         };
-        
-        # App entry for desktop integration
-        apps.default = {
-          type = "app";
-          program = "${audio-switcher}/bin/audio-switcher";
+      };
+      
+      # App entry for desktop integration
+      apps.${system}.default = {
+        type = "app";
+        program = "${audio-switcher}/bin/audio-switcher";
+        meta = {
+          description = "Toggle between built-in audio and SteelSeries headset";
+          platforms = [ "x86_64-linux" ];
         };
-      });
+      };
+    };
 }
