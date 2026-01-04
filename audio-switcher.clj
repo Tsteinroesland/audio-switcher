005a5706ce23252965cc48e2d8b3b012ddecfeb9
Torstein Røsland <tsteinroesland@gmail.com>
2026-01-04 12:54:49 +0100

    Add pixel buds as an option

--- audio-switcher.clj
+++ audio-switcher.clj
@@ -1,6 +1,11 @@
 #!/usr/bin/env bb
 (require '[clojure.java.shell :refer [sh]])
-(require '[clojure.string :refer [includes? split-lines]])
+(require '[clojure.string :refer [includes? split-lines split trim]])
+
+(def devices
+  #{"SteelSeries Arctis 7 Game"
+    "Built-in Audio Analog Stereo"
+    "Torstein's Pixel Buds Pro"})
 
 (def status-lines
   "Call the shell once and store the output lines"
@@ -8,17 +13,46 @@
        (:out)
        (split-lines)))
 
+(def available-sinks
+  (->> status-lines
+       (drop-while #(not (includes? % "Sinks")))
+       (drop 1)
+       (take-while #(re-find #"[0-9]+\." %))
+       (map (fn [dev] (->> dev
+                           (#(split % #"  "))
+                           (filter #(re-find #"[0-9]+\." %))
+                           (first)
+                           (trim)
+                           (#(split % #"\.")))))
+       (map second)
+       (map trim)))
+
 (def current-device
   "Find the current device using the stored lines"
   (->> status-lines
        (drop-while #(not (includes? % "Sinks:")))
        (drop-while #(not (includes? % "*")))
-       (first)))
+       (first)
+       (#(split % #"  "))
+       (filter #(re-find #"[0-9]+\." %))
+       (first)
+       (trim)
+       (#(split % #"\."))
+       (second)
+       (trim)))
 
 (def output-device
   "Determine the target device"
-  (if (includes? current-device "Built-in Audio Analog Stereo")
+  (case current-device
+    "Built-in Audio Analog Stereo"
     "SteelSeries Arctis 7 Game"
+
+    "SteelSeries Arctis 7 Game"
+    (if (some #(includes? % "Torstein's Pixel Buds Pro") available-sinks)
+      "Torstein's Pixel Buds Pro"
+      "Built-in Audio Analog Stereo")
+
+    "Torstein's Pixel Buds Pro"
     "Built-in Audio Analog Stereo"))
 
 (def sink-id
